3eca7d531058e9c8e0ac663aa9fd9c5f890637e0
DSibel <70350525+DSibel@users.noreply.github.com>
2020-12-07 18:22:21 -0500

    commit

---
+++ Sumo-agents/Assets/Scripts/SumoAgent.cs
@@ -0,0 +1,209 @@
+﻿
+using System;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgentsExamples;
+using Unity.MLAgents.Sensors;
+using BodyPart = Unity.MLAgentsExamples.BodyPart;
+using Random = UnityEngine.Random;
+
+public class SumoAgent : Agent
+{
+     [Header("Villan")] 
+     public Transform vill;
+     public GameObject villan;
+     public Transform villbody;
+
+    [Header("Body Parts")] [Space(10)] 
+    public Transform body;
+    public Transform leg0Upper;
+    public Transform leg0Lower;
+    public Transform leg1Upper;
+    public Transform leg1Lower;
+    public Transform leg2Upper;
+    public Transform leg2Lower;
+    public Transform leg3Upper;
+    public Transform leg3Lower;
+
+    OrientationCubeController m_OrientationCube;
+    JointDriveController m_JdController;
+    
+        private Transform startpos;
+      public override void Initialize()
+    {    
+       m_OrientationCube = GetComponentInChildren<OrientationCubeController>();
+       
+        m_JdController = GetComponent<JointDriveController>();
+
+        //Setup each body part
+        m_JdController.SetupBodyPart(body);
+        m_JdController.SetupBodyPart(leg0Upper);
+        m_JdController.SetupBodyPart(leg0Lower);
+        m_JdController.SetupBodyPart(leg1Upper);
+        m_JdController.SetupBodyPart(leg1Lower);
+        m_JdController.SetupBodyPart(leg2Upper);
+        m_JdController.SetupBodyPart(leg2Lower);
+        m_JdController.SetupBodyPart(leg3Upper);
+        m_JdController.SetupBodyPart(leg3Lower);
+        
+    }
+
+public override void OnEpisodeBegin()
+    {
+        foreach (var bodyPart in m_JdController.bodyPartsDict.Values)
+        {
+            bodyPart.Reset(bodyPart);
+        }
+
+       
+        body.rotation = Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0);
+
+        UpdateOrientationObjects();
+       
+        
+    }
+
+public void CollectObservationBodyPart(BodyPart bp, VectorSensor sensor)
+    {
+        //GROUND CHECK
+        sensor.AddObservation(bp.groundContact.touchingGround); // Is this bp touching the ground
+
+        if (bp.rb.transform != body)
+        {
+            sensor.AddObservation(bp.currentStrength / m_JdController.maxJointForceLimit);
+        }
+    }
+
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        var cubeForward = m_OrientationCube.transform.forward;
+
+           
+        //ragdoll's avg vel
+        var avgVel = GetAvgVelocity();
+        sensor.AddObservation(villbody.position);
+        //current ragdoll velocity. normalized
+        sensor.AddObservation(avgVel/100f);
+        //avg body vel relative to cube
+        sensor.AddObservation(m_OrientationCube.transform.InverseTransformDirection(avgVel));
+
+        //rotation delta
+        sensor.AddObservation(Quaternion.FromToRotation(body.forward, cubeForward));
+
+        //Add pos of target relative to orientation cube
+        sensor.AddObservation(m_OrientationCube.transform.InverseTransformPoint(vill.transform.position));
+
+        RaycastHit hit;
+        float maxRaycastDist = 10;
+        if (Physics.Raycast(body.position, Vector3.down, out hit, maxRaycastDist))
+        {
+            sensor.AddObservation(hit.distance / maxRaycastDist);
+        }
+        else
+            sensor.AddObservation(1);
+
+        foreach (var bodyPart in m_JdController.bodyPartsList)
+        {
+            CollectObservationBodyPart(bodyPart, sensor);
+        }
+    }
+
+
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+      
+        // The dictionary with all the body parts in it are in the jdController
+        var bpDict = m_JdController.bodyPartsDict;
+
+        var continuousActions = actionBuffers.ContinuousActions;
+        var i = -1;
+        // Pick a new target joint rotation
+        bpDict[leg0Upper].SetJointTargetRotation(continuousActions[++i], continuousActions[++i], 0);
+        bpDict[leg1Upper].SetJointTargetRotation(continuousActions[++i], continuousActions[++i], 0);
+        bpDict[leg2Upper].SetJointTargetRotation(continuousActions[++i], continuousActions[++i], 0);
+        bpDict[leg3Upper].SetJointTargetRotation(continuousActions[++i], continuousActions[++i], 0);
+        bpDict[leg0Lower].SetJointTargetRotation(continuousActions[++i], 0, 0);
+        bpDict[leg1Lower].SetJointTargetRotation(continuousActions[++i], 0, 0);
+        bpDict[leg2Lower].SetJointTargetRotation(continuousActions[++i], 0, 0);
+        bpDict[leg3Lower].SetJointTargetRotation(continuousActions[++i], 0, 0);
+
+        // Update joint strength
+        bpDict[leg0Upper].SetJointStrength(continuousActions[++i]);
+        bpDict[leg1Upper].SetJointStrength(continuousActions[++i]);
+        bpDict[leg2Upper].SetJointStrength(continuousActions[++i]);
+        bpDict[leg3Upper].SetJointStrength(continuousActions[++i]);
+        bpDict[leg0Lower].SetJointStrength(continuousActions[++i]);
+        bpDict[leg1Lower].SetJointStrength(continuousActions[++i]);
+        bpDict[leg2Lower].SetJointStrength(continuousActions[++i]);
+        bpDict[leg3Lower].SetJointStrength(continuousActions[++i]);
+             AddReward(-.001f);
+    }
+
+
+       void Update()
+    { 
+     
+       if(vill.position.y<-10){
+            Won();
+           }
+        }
+
+    
+
+
+    void UpdateOrientationObjects()
+    {
+        m_OrientationCube.UpdateOrientation(body, vill);
+      
+    }
+
+
+      Vector3 GetAvgVelocity()
+    {
+        Vector3 velSum = Vector3.zero;
+        Vector3 avgVel = Vector3.zero;
+
+        //ALL RBS
+        int numOfRB = 0;
+        foreach (var item in m_JdController.bodyPartsList)
+        {
+            numOfRB++;
+            velSum += item.rb.velocity;
+        }
+
+        avgVel = velSum / numOfRB;
+        return avgVel;
+    }
+
+
+
+public void Won()
+    {
+        AddReward(1f);
+         SumoAgent other=(SumoAgent) villan.GetComponent(typeof(SumoAgent));
+         other.Lost();
+        EndEpisode();
+       
+    }
+
+public void Lost(){
+    Debug.Log("Lost");
+         AddReward(-1f);
+        
+         EndEpisode();
+    }
+
+     
+
+
+}
+
+
+
+
+
+
+  
